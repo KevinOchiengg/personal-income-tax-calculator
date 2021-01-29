0e4d5e36bc0cec46eb01e3d17bf80cef5c782a29
Kevin573 <kevinochieng932@gmail.com>
2021-01-29 14:07:06 +0300

    a C program to calculate net income

--- main.c
+++ main.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
 
 int main(){
-int number, netSalary,  allowance, salary, grossPay, paye;
+long int  netSalary,  allowance, salary, grossPay, paye;
+int number;
 char name;
    
    printf("Enter Your Employee number:\n");
-   scanf ("%d",&number);
+   scanf ("%ld",&number);
    printf("Enter Your Name:\n");
    scanf ("%s",&name);
    printf("Enter Your Salary:\n");
-   scanf ("%d",&salary);
+   scanf ("%ld",&salary);
    printf("Enter Your Allowance:\n");
-   scanf ("%d",&allowance);
+   scanf ("%ld",&allowance);
 
    grossPay = allowance + salary;
 
-      if(grossPay <= 10000){
+      if(grossPay <= 10000)
        paye = grossPay * 0;
-       netSalary = grossPay - paye;
-        
-      }
-      else if ((grossPay >= 10001) && (grossPay <= 60000)){
-         paye = (grossPay-10000) * 0.1;
-         netSalary=grossPay - paye;
-        
-      } 
-      else if ((grossPay >= 60001) && (grossPay <= 110000)){
-         paye = (50000 * 0.1) + (grossPay-60000) * 0.15;
-         netSalary=grossPay - paye;
-        
-      }
-        
-      else if((grossPay >= 110001) && (grossPay <= 160000)){
-         paye = (50000 * 0.1) + (50000 * 0.1) + (grossPay - 110000 ) * 0.20;
-         netSalary=grossPay - paye;
-        
-      }
-      else if((grossPay >= 160001) && (grossPay <= 210000)){
-         paye = (50000 * 0.1) + (50000 * 0.1) + (50000 * 0.2) + (grossPay - 160000) * 0.25;
-         netSalary=grossPay - paye;
-        
-      }
-        
-      else {
-         paye = (50000 * 0.1) + (50000 * 0.1) + (50000 * 0.2) + (50000 * 0.25) + (grossPay - 160000) * 0.3;
-         netSalary=grossPay - paye;
-      }
        
-      //  printf("pf : %d\n", number);
-      //  printf("Name : %s\n", name);
-      //  printf("p.a.y.e : %d\n", paye);
-       printf("Your Net Salary after tax deduction is : %d\n", netSalary);
+      else if (grossPay <= 60000)
+         paye = (grossPay-10000) * 10/100;
+         
+      else if (grossPay <= 110000)
+         paye = (50000 * 10/100) + (grossPay-60000) * 15/100;
+         
+      else if(grossPay <= 160000)
+         paye = (50000 * 10/100) + (50000 * 15/100) + (grossPay - 110000 ) * 20 / 100;
+         
+        
+      else if(grossPay <= 210000)
+         paye = (50000 * 10 / 100) + (50000 * 15 / 100) + (50000 * 20 / 100) + (grossPay - 160000) * 25/100;
+        
+      else 
+          paye = (50000 * 10 / 100) + (50000 * 15 / 100) + (50000 * 20 / 100) + (50000 * 25 / 100) + ((grossPay - 210000) * 30 / 100);
+         
+      
+       
+      netSalary=grossPay - paye;
+      printf("Net Salary: KSH.%ld\n", netSalary);
+    
         
  return 0;
 }
